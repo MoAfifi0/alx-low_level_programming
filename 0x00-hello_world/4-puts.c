94b82a055e2bed99a3e5566f3072a206c4778001
Mohamed Afifi <m.afifi00000@gmail.com>
2022-10-13 18:22:40 +0200

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#include  <stdio.h>
+#include <stdio.h>
 
 /**
  *  * main - Entry point
- *   * Description: C program that prints a function followed by a new line
- *    * Return: 0 if successful
+ *   *
+ *    * Return: Always 0 (Success)
  *     */
-
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
